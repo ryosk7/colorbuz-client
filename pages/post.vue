726a93aadb1f723943797ce4835e2287da8d6713
ryosk7 <uchiryo7@gmail.com>
2019-08-29 05:31:18 +0900

    post.vueを追加

---
+++ pages/post.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <li v-for="post in posts" :key="post.id">
+      <p>hoge</p>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+      newPost: ''
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  methods: {
+    fetchPosts() {
+      this.$axios.get('/posts').then(
+        (response) => {
+          for (let i = 0; i < response.data.posts.length; i++) {
+            this.posts.push(response.data.posts[i])
+          }
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    }
+  }
+}
+</script>
